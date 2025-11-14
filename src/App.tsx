0cad63be2b66d5171e1db4a458c6e9a77b387d71
giga <beppe_1989@yahoo.it>
2025-11-14 12:30:50 +0100

    cmd: npx shadcn@latest add button

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import { Button } from "@/components/ui/button"
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
 import viteLogo from '/vite.svg'
+import { useId, useState } from 'react'
+
+import reactLogo from './assets/react.svg'
+
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+const id = useId()
+
+
+  return (
+    <div className="flex min-h-svh flex-col items-center justify-center">
+      <Button>Click me</Button>
+      <div className='w-full max-w-xs space-y-2'>
+        <Label htmlFor={id}>Input with label</Label>
+        <Input id={id} type='email' placeholder='Email address' />
+      </div>
+    </div>
+  )
 
   return (
     <>
